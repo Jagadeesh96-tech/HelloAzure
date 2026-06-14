6e86869e9f13e62544b6c446bbe4ffd5aa0ac5f7
Jagadeesh <jagadeeshkumarvakadi@gmail.com>
2026-06-14 11:31:39 +0530

    Add project files.

---
+++ HelloAzureApi/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloAzureApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World from Azure CI/CD API 🚀");
+        }
+
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok("API is running fine ✅");
+        }
+    }
+}
